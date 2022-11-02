90ae205a9e669d81d3fc7a1f72a53f84ac24034c
JomielE <JomielE@MBTMNL-JOMIEE>
2022-11-02 11:13:13 +0800

    11/2/2022 Update -Confiscated License -Impounded Vehicle -Clerk Dashboard -Statistics

--- CAPSTONE/Controllers/SystemsController.cs
+++ CAPSTONE/Controllers/SystemsController.cs
@@ -43,6 +43,69 @@ namespace CAPSTONE.Controllers
             return View(userControl);
         }
 
+        public ActionResult Statics(string id)
+        {
+            UserControl userControl = new UserControl();
+
+            SystemsRepository systemsRepository = new SystemsRepository();
+
+
+            userControl.currentuser = systemsRepository.GetCurrentUser(id);
+
+            if (userControl.currentuser.acnttype == "" || userControl.currentuser.acnttype == null) return RedirectToAction("Index", "Login");
+
+            userControl.menulist = systemsRepository.GetMenu(userControl.currentuser.acnttype);
+
+            return View(userControl);
+        }
+
+        public ActionResult Daily(string id)
+        {
+            UserControl userControl = new UserControl();
+
+            SystemsRepository systemsRepository = new SystemsRepository();
+
+
+            userControl.currentuser = systemsRepository.GetCurrentUser(id);
+
+            if (userControl.currentuser.acnttype == "" || userControl.currentuser.acnttype == null) return RedirectToAction("Index", "Login");
+
+            userControl.menulist = systemsRepository.GetMenu(userControl.currentuser.acnttype);
+
+            return View(userControl);
+        }
+        public ActionResult ConfiscatedLicense(string id)
+        {
+            UserControl userControl = new UserControl();
+
+            SystemsRepository systemsRepository = new SystemsRepository();
+
+
+            userControl.currentuser = systemsRepository.GetCurrentUser(id);
+
+            if (userControl.currentuser.acnttype == "" || userControl.currentuser.acnttype == null) return RedirectToAction("Index", "Login");
+
+            userControl.menulist = systemsRepository.GetMenu(userControl.currentuser.acnttype);
+
+            return View(userControl);
+        }
+
+        public ActionResult ImpoundedVehicles(string id)
+        {
+            UserControl userControl = new UserControl();
+
+            SystemsRepository systemsRepository = new SystemsRepository();
+
+
+            userControl.currentuser = systemsRepository.GetCurrentUser(id);
+
+            if (userControl.currentuser.acnttype == "" || userControl.currentuser.acnttype == null) return RedirectToAction("Index", "Login");
+
+            userControl.menulist = systemsRepository.GetMenu(userControl.currentuser.acnttype);
+
+            return View(userControl);
+        }
+
         public ActionResult About(string id)
         {
             UserControl userControl = new UserControl();
@@ -159,6 +222,11 @@ namespace CAPSTONE.Controllers
             SystemsRepository systemsRepository = new SystemsRepository();
             return systemsRepository.getreport();
         }
+        public string getdailyreport()
+        {
+            SystemsRepository systemsRepository = new SystemsRepository();
+            return systemsRepository.getdailyreport();
+        }
         public string get_total()
         {
             SystemsRepository systemsRepository = new SystemsRepository();
@@ -203,6 +271,21 @@ namespace CAPSTONE.Controllers
         {
             SystemsRepository systemsRepository =new SystemsRepository();
             return systemsRepository.delete_citation(id);
+        }
+        public string getuserfullname(string acntuid)
+        {
+            SystemsRepository systemsRepository = new SystemsRepository();
+            return systemsRepository.GetUserFullName(acntuid);
+        }
+        public string get_statistics()
+        {
+            SystemsRepository systemsRepository = new SystemsRepository();
+            return systemsRepository.proc_get_statistics();
+        }
+        public string prop_get_confiscated_license()
+        {
+            SystemsRepository systemsRepository = new SystemsRepository();
+            return systemsRepository.prop_get_confiscated_license();
         }
     }
 }
