3ced1f1f8d443fba0c56aa2eb097ef1aea66933d
JomielE <JomielE@MBTMNL-JOMIEE>
2022-12-05 12:07:18 +0800

    Dec 5 First Update

---
+++ CAPSTONEDB/queries/tblmenu.sql
@@ -0,0 +1,16 @@
+select * from tblticketcitation
+
+
+select * from tblAccounts
+
+select * from tblmenu
+
+--insert new
+--insert into tblmenu(text,acnttype,action,controller,icon,tagid) values('Records of Setteled Apprehensions', 'CLERK', 'SettledApprehensions','Systems','fa-check-square-o','tg-settled')
+
+--insert about
+--delete tblmenu where id=20
+--insert into tblmenu(text,acnttype,action,controller,icon,tagid) values('About', 'CLERK', 'About','Systems','fa-info','tg-about')
+
+
+--update tblmenu set Text='Settled Apprehensions' where id =21
